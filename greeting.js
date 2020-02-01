48d139a1d4e0608dec449aa5746393f6e1f88a9b
InKyoJeong <viapolar@naver.com>
2020-02-01 14:14:34 +0900

    #3 Save the User Name to LocalStorage

--- greeting.js
+++ greeting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paingGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paingGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,7 +30,7 @@ function paingGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    // she is not
+    askForName();
   } else {
     //she is
     paingGreeting(currentUser);
